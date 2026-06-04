1f96c90a5499562391d24409d6b46805fd9ed3ac
yogeshrpdigital <yogeshrpdigital@gmail.com>
2026-06-04 16:29:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from mcp.server.fastmcp import FastMCP
+import uvicorn
+import os
 
 mcp = FastMCP("Dialogflow MCP")
 
 @mcp.tool()
 def hello():
-    return "Hello from MCP"
+    return "Hello from MCP Server"
+
+app = mcp.streamable_http_app()
 
 if __name__ == "__main__":
-    mcp.run()
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
