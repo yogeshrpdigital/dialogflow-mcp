0beb68b45c4b13360cf65b76623d34da377fc986
yogeshrpdigital <yogeshrpdigital@gmail.com>
2026-06-04 17:19:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,29 +1,7 @@
-import os
+
 from mcp.server.fastmcp import FastMCP
-from mcp.server.transport_security import TransportSecuritySettings
 
-def require_env(key: str):
-    value = os.getenv(key)
-    if not value:
-        raise ValueError(f"Missing env var: {key}")
-    return value
-
-mcp = FastMCP(
-    "Dialogflow MCP",
-    transport_security=TransportSecuritySettings(
-        enable_dns_rebinding_protection=False,
-        allowed_hosts=[],
-        allowed_origins=[]
-    )
-)
-
-@mcp.tool()
-def get_agent_info():
-    return {
-        "project": require_env("PROJECT_ID"),
-        "location": require_env("LOCATION"),
-        "agent": require_env("AGENT_ID")
-    }
+mcp = FastMCP("Dialogflow MCP")
 
 @mcp.tool()
 def health():
